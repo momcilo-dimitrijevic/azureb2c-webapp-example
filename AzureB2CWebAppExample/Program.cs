edbe787848d82d8d78bece5d521363e9bcd05ab4
Momcilo Dimitrijevic <momcilo.dimitrijevic@zoho.no>
2023-02-02 23:21:19 +0100

    refactor: use default oidc middleware
    WIP: Authentication is working, User.Identity is not getting populated

--- AzureB2CWebAppExample/Program.cs
+++ AzureB2CWebAppExample/Program.cs
@@ -1,7 +1,10 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +16,33 @@ builder.Services
 builder.Services.AddRazorPages();
 
 builder.Services
-    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(
-            configurationSection: builder.Configuration.GetSection("AzureADB2C"),
-            subscribeToOpenIdConnectMiddlewareDiagnosticsEvents: true);
+    .AddAuthentication(opts =>
+    {
+        opts.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        opts.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        opts.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme,opts =>
+    {
+        var config = builder.Configuration.GetSection("AzureADB2C").Get<OpenIdConnectOptions>();
+        var policyBasedAuthority = builder.Configuration.GetSection("AzureADB2CPolicyAuthority").Get<string>();
+        
+        opts.Authority = policyBasedAuthority;
+        opts.ClientId = config.ClientId;
+        opts.ClientSecret = config.ClientSecret;
+        opts.ResponseType = OpenIdConnectResponseType.Code;
+        opts.ResponseMode = OpenIdConnectResponseMode.FormPost;
+
+        opts.Events.OnTokenResponseReceived += context =>
+        {
+            Console.WriteLine("-----------------------Access token----------------------------");
+            Console.WriteLine(JsonSerializer.Serialize(context.TokenEndpointResponse.IdToken));
+            
+            Console.WriteLine(context.HttpContext.User.Identity?.Name);
+            return Task.CompletedTask;
+        };
+    });
 
 builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
 {
@@ -46,6 +72,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
